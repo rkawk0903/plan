830293fce9891976761227912e619e057789a8a4
rkawk0903 <rkawk0903@gmail.com>
2026-09-09 11:47:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,14 @@
-const CACHE_NAME = 'wedding-planner-v1';
-
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
+const CACHE_NAME = 'wedding-planner-v3';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
